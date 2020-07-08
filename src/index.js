206f67a372b8cf590b4f16dd32c983fdb29b88b9
陈成 <cheng@tanmer.com>
2020-07-08 17:00:43 +0800

    add inline_strikeout code

---
+++ src/index.js
@@ -0,0 +1,189 @@
+/**
+ * Build styles
+ */
+require('./index.css').toString();
+
+/**
+ * InlineStrikeout Tool for the Editor.js
+ *
+ * Allows to wrap inline fragment and style it somehow.
+ */
+class InlineStrikeout {
+  /**
+   * Class name for term-tag
+   *
+   * @type {string}
+   */
+  static get CSS() {
+    return 'cdx-strikeout';
+  };
+
+  /**
+   * @param {{api: object}}  - Editor.js API
+   */
+  constructor({ api }) {
+    this.api = api;
+
+    /**
+     * Toolbar Button
+     *
+     * @type {HTMLElement|null}
+     */
+    this.button = null;
+
+    /**
+     * Tag represented the term
+     *
+     * @type {string}
+     */
+    this.tag = 'DEL';
+
+    /**
+     * CSS classes
+     */
+    this.iconClasses = {
+      base: this.api.styles.inlineToolButton,
+      active: this.api.styles.inlineToolButtonActive
+    };
+  }
+
+  /**
+   * Specifies Tool as Inline Toolbar Tool
+   *
+   * @returns {boolean}
+   */
+  static get isInline() {
+    return true;
+  }
+
+  /**
+   * Create button element for Toolbar
+   *
+   * @returns {HTMLElement}
+   */
+  render() {
+    this.button = document.createElement('button');
+    this.button.type = 'button';
+    this.button.classList.add(this.iconClasses.base);
+    this.button.innerHTML = this.toolboxIcon;
+
+    return this.button;
+  }
+
+  /**
+   * Wrap/Unwrap selected fragment
+   *
+   * @param {Range} range - selected fragment
+   */
+  surround(range) {
+    if (!range) {
+      return;
+    }
+
+    const termWrapper = this.api.selection.findParentTag(this.tag, InlineStrikeout.CSS);
+
+    /**
+     * If start or end of selection is in the highlighted block
+     */
+    if (termWrapper) {
+      this.unwrap(termWrapper);
+    } else {
+      this.wrap(range);
+    }
+  }
+
+  /**
+   * Wrap selection with term-tag
+   *
+   * @param {Range} range - selected fragment
+   */
+  wrap(range) {
+    /**
+     * Create a wrapper for highlighting
+     */
+    const strikeout = document.createElement(this.tag);
+
+    strikeout.classList.add(InlineStrikeout.CSS);
+
+    /**
+     * SurroundContent throws an error if the Range splits a non-Text node with only one of its boundary points
+     *
+     * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Range/surroundContents}
+     *
+     * // range.surroundContents(span);
+     */
+    strikeout.appendChild(range.extractContents());
+    range.insertNode(strikeout);
+
+    /**
+     * Expand (add) selection to highlighted block
+     */
+    this.api.selection.expandToTag(strikeout);
+  }
+
+  /**
+   * Unwrap term-tag
+   *
+   * @param {HTMLElement} termWrapper - term wrapper tag
+   */
+  unwrap(termWrapper) {
+    /**
+     * Expand selection to all term-tag
+     */
+    this.api.selection.expandToTag(termWrapper);
+
+    const sel = window.getSelection();
+    const range = sel.getRangeAt(0);
+
+    const unwrappedContent = range.extractContents();
+
+    /**
+     * Remove empty term-tag
+     */
+    termWrapper.parentNode.removeChild(termWrapper);
+
+    /**
+     * Insert extracted content
+     */
+    range.insertNode(unwrappedContent);
+
+    /**
+     * Restore selection
+     */
+    sel.removeAllRanges();
+    sel.addRange(range);
+  }
+
+  /**
+   * Check and change Term's state for current selection
+   */
+  checkState() {
+    const termTag = this.api.selection.findParentTag(this.tag, InlineStrikeout.CSS);
+
+    this.button.classList.toggle(this.iconClasses.active, !!termTag);
+  }
+
+  /**
+   * Get Tool icon's SVG
+   *
+   * @returns {string}
+   */
+  get toolboxIcon() {
+    return require('./assets/strikeout.svg').default;
+  }
+
+  /**
+   * Sanitizer rule
+   *
+   * @returns {{del: {class: string}}}
+   */
+  static get sanitize() {
+    return {
+      del: {
+        class: InlineStrikeout.CSS,
+      },
+    };
+  }
+}
+
+module.exports = InlineStrikeout;
